00dad06317aab37e6b2dd2d9df0c5b975e762397
gustavozreis <gzreis@gmail.com>
2023-01-26 13:40:32 -0300

    Exercício abstrações.

---
+++ CURSO_EDU/AULA_225/log.py
@@ -0,0 +1,46 @@
+# Abstração
+
+from asyncore import write
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+class Log:
+    def _log(self, msg):
+        raise NotImplementedError(
+            'Implemente o método log'
+        )
+    
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+    
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')    
+    
+        
+class LogFileMixin(Log):
+    def _log(self, msg):
+        msg_format = f'{msg} ({self.__class__.__name__})'
+        
+        with open(LOG_FILE, 'a') as file:
+            file.write(msg_format)
+            file.write('\n')
+        
+        print(msg_format)        
+        
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+        
+
+if __name__ == '__main__':        
+    lp = LogPrintMixin()
+    lp.log_error('qualquer coisa')
+    lp.log_success('sucesso')
+    
+    lf = LogFileMixin()
+    lf.log_error('qualquer coisa')
+    lf.log_success('sucesso')
+    
+    
