b0b1e33655f630969241a5257df79785ced67706
gustavozreis <gzreis@gmail.com>
2023-01-27 17:51:32 -0300

    Classe decoradora 02.

--- CURSO_EDU/decorators_class_functions.py
+++ CURSO_EDU/decorators_class_functions.py
@@ -2,7 +2,7 @@ def change_speed(cls):
     def new_my_speed(self):
         return self.velocity * 50
     cls.my_speed = new_my_speed
-    return cls
+    return cls        
 
 
 @change_speed
@@ -16,7 +16,18 @@ class Car:
     
 
 my_car = Car('Focus', 20)
-print(my_car.my_speed())
+# print(my_car.my_speed())
 
 
+class Multiplicar:
+    def __init__(self, func) -> None:
+        self.func = func
+        
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs) * 10
 
+@Multiplicar
+def somar(x, y):
+    return x + y
+
+print(somar(2,5))
--- CURSO_EDU/testes.py
+++ CURSO_EDU/testes.py
@@ -1,21 +1,2 @@
-class Person:
-    
-    __foo = 2
-    
-    def __init__(self, name, age) -> None:
-        self.name = name
-        self.age = age
-        self.__foo = 2
-        
-    def test(self):
-        print(self.__foo)
-        
-    def __private(self):
-        print(2)
-        
-    
-gustavo = Person('gustavo', 35)    
-    
-print(gustavo.test())
-
-print(gustavo.__foo)
+for i in range(1, 11):
+    print(i)
